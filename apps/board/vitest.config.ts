894d221f34a5162363cfd952bafaa97647dc3701
Ian Welerson <welerson.ian@gmail.com>
2026-04-14 16:35:49 -0600

    setup: add vitest to board app

---
+++ apps/board/vitest.config.ts
@@ -0,0 +1,33 @@
+import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+
+const EXCLUDE_PATTERN = [
+  'node_modules/**',
+  'coverage/**',
+  'public/**',
+  'vitest/**',
+  '.*/**',
+  '**/*.css',
+  '**/*.svg',
+  '**/*.config.{ts,js}',
+];
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    setupFiles: ['./vitest/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      exclude: EXCLUDE_PATTERN,
+    },
+    include: ['app/**/*.{test,spec}.{ts,tsx}'],
+    exclude: EXCLUDE_PATTERN,
+    browser: {
+      provider: playwright(),
+      enabled: true,
+      headless: true,
+      instances: [{ browser: 'chromium' }],
+    },
+  },
+});
