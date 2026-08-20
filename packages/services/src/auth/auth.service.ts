a758bc0f969ef00b1dc308ab7a4a6e5d27029261
Ian Welerson <welerson.ian@gmail.com>
2026-08-19 22:00:57 -0600

    feat(services): add the password reset calls to the auth service

--- packages/services/src/auth/auth.service.ts
+++ packages/services/src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { API_ROUTES } from '@ordre/core/constants';
 import {
   AuthSuccessResponseSchema,
+  type RequestPasswordResetInput,
+  type ResetPasswordInput,
   type RevokeSessionInput,
   SessionResponseSchema,
   type SignInInput,
@@ -20,6 +22,10 @@ export const createAuthService = (http: HttpClient) => ({
   signIn: (payload: SignInInput) =>
     http.post(API_ROUTES.auth.signIn, payload, SignInResponseSchema),
   signOut: () => http.post(API_ROUTES.auth.signOut, undefined, AuthSuccessResponseSchema),
+  requestPasswordReset: (payload: RequestPasswordResetInput) =>
+    http.post(API_ROUTES.auth.requestPasswordReset, payload, AuthSuccessResponseSchema),
+  resetPassword: (payload: ResetPasswordInput) =>
+    http.post(API_ROUTES.auth.resetPassword, payload, AuthSuccessResponseSchema),
   getSession: () => http.get(API_ROUTES.auth.session, SessionResponseSchema),
   revokeSession: (payload: RevokeSessionInput) =>
     http.post(API_ROUTES.auth.revokeSession, payload, AuthSuccessResponseSchema),
